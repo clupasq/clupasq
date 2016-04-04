996f2d621ee5b08b23fbaf5971aaebd2540699fc
Cristian Lupascu <cristi.lupascu@gmail.com>
2016-04-04 16:45:37 +0300

    added options (-h and -o) to the setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
+while [[ $# > 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -h|--help)
+      HELP=Y
+      ;;
+    -o|--overwrite)
+      OVERWRITE=Y
+      ;;
+    *)
+      echo "Unknown option: $1. Use -h/--help to get help."
+      ;;
+  esac
+  shift
+done
+
+if [[ $HELP == "Y" ]]; then
+  echo "USAGE"
+  echo "  ./setup.sh [flags]"
+  echo
+  echo "FLAGS"
+  echo "  -h | --help        - Print this help"
+  echo "  -o | --overwrite   - Overwrite files if they already exist"
+  echo
+
+  exit
+fi
+
 function installVundleForVimPlugins {
   vundle_dir="$HOME/.vim/bundle/Vundle.vim"
   if [ ! -d "$vundle_dir" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git $vundle_dir 
   else
-    echo "Vundle appears to be already installed."
+    echo "Vundle appears to be already installed - skipping."
   fi
 }
 
@@ -20,7 +50,13 @@ function createDotfileLinks {
       ln -s "$source_dir/$f" $dest 
       echo "$f - linked OK"
     else
-      echo "$f - already present (not overwritten)"
+      if [[ $OVERWRITE == "Y" ]]; then
+        rm -rf $dest
+        ln -s "$source_dir/$f" $dest 
+        echo "$f - overwritten!"
+      else
+        echo "$f - already present (not overwritten; use -o to overwrite)"
+      fi
     fi
   done
 }
